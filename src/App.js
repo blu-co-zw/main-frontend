a4dd9f99d05136a4a699167a937994b7f890ea0c
Blu.Inc <blucozw@gmail.com>
2021-01-03 21:53:41 +0200

    setting up website essentials
    - add custom favicon.ico, logos and css styles

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="left-white-box"></div>
+      <div class="right-white-box">
+        <span class="number-three">03</span>
+      </div>
+      <span class="bottom-left-text">blu.co.zw</span>
+    </div>
+  );
+}
+
+export default App;
