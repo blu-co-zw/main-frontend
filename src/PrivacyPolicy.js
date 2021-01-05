95ad27f63866aa1aab5ea47905a467aea03a3acd
Blu.Inc <blucozw@gmail.com>
2021-01-05 03:45:04 +0200

    edit privacy policy
    - change formatting and edit text

--- src/PrivacyPolicy.js
+++ src/PrivacyPolicy.js
@@ -3,12 +3,12 @@ function PrivacyPolicy() {
     document.body.style.color = "#3c4043"
     return (
         <div class="privacy-policy">
-            <h1>PRIVACY NOTICE</h1>
+            <h1>PRIVACY POLICY</h1>
             <p>Last updated January 1, 2021</p>
-            <p>Thank you for using Book Keeper. We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy notice, or our practices with regards to your personal information, please contact us at: blucozw@gmail.com. </p>
-            <p>When you and more generally, use any of our services to sort and manage your transactions and other related services, we appreciate that you are trusting us with your information. We take your privacy very seriously. In this privacy notice, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy notice that you do not agree with, please discontinue use of our Services immediately. </p>
-            <p>This privacy notice applies to all information collected through our Services (which, as described above, includes our ), as well as, any related services, sales, marketing or events. </p>
-            <p>Please read this privacy notice carefully as it will help you understand what we do with the information that we collect. </p>
+            <p>Thank you for using Book Keeper. We are committed to protecting your personal information and your right to privacy. If you have any questions or concerns about this privacy policy, or our practices with regards to your personal information, please contact us at: blucozw@gmail.com. </p>
+            <p>When you and more generally, use any of our services to sort and manage your transactions and other related services. We appreciate that you are trusting us with your information. We take your privacy very seriously. In this privacy policy, we seek to explain to you in the clearest way possible what information we collect, how we use it and what rights you have in relation to it. We hope you take some time to read through it carefully, as it is important. If there are any terms in this privacy policy that you do not agree with, please discontinue use of our services immediately. </p>
+            <p>This privacy policy applies to all information collected through our services (which, as described above, includes Book Keeper), as well as, any related services, sales, marketing or events. </p>
+            <p><b>Please read this privacy policy carefully as it will help you understand what we do with the information that we collect. </b></p>
 
             <h3>TABLE OF CONTENTS </h3>
             <ol>
@@ -21,18 +21,17 @@ function PrivacyPolicy() {
                 <li>WHAT ARE YOUR PRIVACY RIGHTS?</li>
                 <li>CONTROLS FOR DO-NOT-TRACK FEATURES</li>
                 <li>DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</li>
-                <li>DO WE MAKE UPDATES TO THIS NOTICE?</li>
-                <li>HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</li>
+                <li>DO WE MAKE UPDATES TO THIS POLICY?</li>
+                <li>HOW CAN YOU CONTACT US ABOUT THIS POLICY?</li>
             </ol>
 
             <h3>1. WHAT INFORMATION DO WE COLLECT? </h3>
-            <p>Do we share, collect or store your SMS' or transactions  </p>
-            <i><b>In Short:</b> No we don't share, collect, store your SMS' or transactions this information is stored on your physical device and is not shared with anyone else. </i>
-            <p>Book Keeper is intended to categorise or sort your EcoCash transactions on your physical device and no SMS or transaction is transferred to our servers or any third party unless the you the user takes decided action(s) to do so.</p>
-            <p>Please note: this is subject to change in the future as we add more features to the mobile application or as a results of change in leadership or ownership.</p>
-            <p>Information automatically collected</p>
-            <i><b>In Short:</b> Some information — such as your Internet Protocol (IP) address and/or browser and device characteristics — is collected automatically when you visit our .</i>
-            <p>We automatically collect certain information when you use Book Keeper. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use our and other technical information. This information is primarily needed to maintain the security and operation of our , and for our internal analytics and reporting purposes.</p>
+            <p><b>Do we share, collect or store your SMSes or transactions  </b></p>
+            <i><b>In Short:</b> No we do not share, collect, store your SMSes or transactions. This information is stored on your physical device and is not shared with anyone else. </i>
+            <p>Book Keeper is intended to categorise or sort your EcoCash transactions on your physical device and no SMS or transaction is transferred to our servers or any third party.</p>
+            <p><b>Information automatically collected</b></p>
+            <i><b>In Short:</b> Some information — such as your Internet Protocol (IP) address and/or browser and device characteristics — is collected automatically when you use Book Keeper.</i>
+            <p>We automatically collect certain information when you use Book Keeper. This information does not reveal your specific identity (like your name or contact information) but may include device and usage information, such as your IP address, browser and device characteristics, operating system, language preferences, referring URLs, device name, country, location, information about how and when you use Book Keeper and other technical information. This information is primarily needed to maintain the security and operation of our services, and for our internal analytics and reporting purposes.</p>
             <p>Like many businesses, we also collect information through cookies and similar technologies.</p>
 
             <h3>2. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h3>
@@ -42,7 +41,7 @@ function PrivacyPolicy() {
 
             <ul>
                 <li>Business Transfers. We may share or transfer your information in connection with, or during negotiations of, any merger, sale of company assets, financing, or acquisition of all or a portion of our business to another company.</li>
-                <li>Affiliates. We may share your information with our affiliates, in which case we will require those affiliates to honor this privacy notice. Affiliates include our parent company and any subsidiaries, joint venture partners or other companies that we control or that are under common control with us.</li>
+                <li>Affiliates. We may share your information with our affiliates, in which case we will require those affiliates to honor this privacy policy. Affiliates include our parent company and any subsidiaries, joint venture partners or other companies that we control or that are under common control with us.</li>
                 <li>Business Partners. We may share your information with our business partners to offer you certain products, services or promotions.</li>
             </ul>
 
@@ -50,19 +49,19 @@ function PrivacyPolicy() {
             <i><b>In Short:</b> We may use cookies and other tracking technologies to collect and store your information.</i>
             <p>We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies.</p>
 
-            <p>4. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</p>
+            <h3>4. IS YOUR INFORMATION TRANSFERRED INTERNATIONALLY?</h3>
             <i><b>In Short:</b> We may transfer, store, and process your information in countries other than your own.</i>
             <p>Our servers location will change depending on your business needs. If you are accessing Book Keeper from inside or outside Zimbabwe, please be aware that your information may be transferred to, stored, and processed by us in our facilities and by those third parties with whom we may share your personal information (see "WILL YOUR INFORMATION BE SHARED WITH ANYONE?" above), in and other countries.</p>
-            <p>If you are a resident in the European Economic Area, then these countries may not necessarily have data protection laws or other similar laws as comprehensive as those in your country. We will however take all necessary measures to protect your personal information in accordance with this privacy notice and applicable law.</p>
+            <p>If you are a resident in the European Economic Area, then these countries may not necessarily have data protection laws or other similar laws as comprehensive as those in your country. We will however take all necessary measures to protect your personal information in accordance with this privacy policy and applicable law.</p>
 
             <h3>5. HOW LONG DO WE KEEP YOUR INFORMATION?</h3>
-            <i><b>In Short:</b> We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice unless otherwise required by law.</i>
-            <p>We will only keep your information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements).</p>
+            <i><b>In Short:</b> We keep your information for as long as necessary to fulfill the purposes outlined in this privacy policy unless otherwise required by law.</i>
+            <p>We will only keep your information for as long as it is necessary for the purposes set out in this privacy policy, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements).</p>
             <p>When we have no ongoing legitimate business need to process your information, we will either delete such information, or, if this is not possible (for example, because your information has been stored in backup archives), then we will securely store your information and isolate it from any further processing until deletion is possible.</p>
 
             <h3>6. DO WE COLLECT INFORMATION FROM MINORS?</h3>
             <i><b>In Short:</b> We do not knowingly collect data from or market to children under 18 years of age.</i>
-            <p>We do not knowingly solicit data from or market to children under 18 years of age. By using Book Keeper, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the . If we learn that personal information from users less than 18 years of age has been collected, we will take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at blucozw@gmail.com.</p>
+            <p>We do not knowingly solicit data from or market to children under 18 years of age. By using Book Keeper, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the . If we learn that personal information from users less than 18 years of age has been collected, we will take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at: blucozw@gmail.com.</p>
 
             <h3>7. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
             <i><b>In Short:</b> You may review, change, or terminate your account at any time.</i>
@@ -71,19 +70,19 @@ function PrivacyPolicy() {
             <p>Opting out of email marketing: You can unsubscribe from our marketing email list at any time by clicking on the unsubscribe link in the emails that we send or by contacting us using the details provided below. You will then be removed from the marketing email list — however, we may still communicate with you, for example to send you service-related emails that are necessary for the administration and use of your account, to respond to service requests, or for other non-marketing purposes.</p>
 
             <h3>8. CONTROLS FOR DO-NOT-TRACK FEATURES</h3>
-            <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice</p>
+            <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy policy.</p>
 
             <h3>9. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</h3>
             <i><b>In Short:</b> Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.</i>
             <p>California Civil Code Section 1798.83, also known as the "Shine The Light" law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.</p>
             <p>If you are under 18 years of age, reside in California, and have a registered account with , you have the right to request removal of unwanted data that you publicly post on the . To request removal of such data, please contact us using the contact information provided below, and include the email address associated with your account and a statement that you reside in California. We will make sure the data is not publicly displayed on the , but please be aware that the data may not be completely or comprehensively removed from all our systems (e.g. backups, etc.).</p>
 
-            <h3>10. DO WE MAKE UPDATES TO THIS NOTICE?</h3>
-            <i><b>In Short:</b> Yes, we will update this notice as necessary to stay compliant with relevant laws.</i>
-            <p>We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.</p>
+            <h3>10. DO WE MAKE UPDATES TO THIS POLICY?</h3>
+            <i><b>In Short:</b> Yes, we will update this policy as necessary to stay compliant with relevant laws.</i>
+            <p>We may update this privacy policy from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy policy, we may notify you either by prominently posting a policy of such changes or by directly sending you a notification. We encourage you to review this privacy policy frequently to be informed of how we are protecting your information.</p>
 
-            <h3>11. HOW CAN YOU CONTACT US ABOUT THIS NOTICE? </h3>
-            <p>If you have questions or comments about this notice, you may email us at: blucozw@gmail.com </p>
+            <h3>11. HOW CAN YOU CONTACT US ABOUT THIS POLICY? </h3>
+            <p>If you have questions or comments about this policy, you may email us at: blucozw@gmail.com </p>
 
         </div>
     )
